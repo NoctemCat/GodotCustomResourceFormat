e20ed1c874726e6fecba8a3493615b7456ac5614
NoctemCat <ya7kolya@gmail.com>
2024-10-06 01:15:41 +0900

    editor test

--- Node2d.cs
+++ Node2d.cs
@@ -4,23 +4,5 @@ namespace NoctemCat.TestLoader;
 
 public partial class Node2d : Node2D
 {
-	[Export]
-	public CustomData Data;
 
-	public override void _Ready()
-	{
-		var resource = ResourceLoader.Load<CustomData>("res://new_resource.ints");
-		GD.PrintS("From load:", resource);
-		resource.Ints = [737, 999, 300];
-		ResourceSaver.Save(resource, "res://new_resource.ints");
-
-		GD.PrintS("From export:", Data);
-		Data.Ints = [1, 2, 9, 10];
-		ResourceSaver.Save(Data, "res://new_resource_data.ints");
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
 }
